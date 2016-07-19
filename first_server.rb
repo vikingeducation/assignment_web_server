60695391dbc0f04b60c1d6ce6f138d8e31a63c46
Mike Lee <asackofwheat@gmail.com>
2016-07-19 12:36:02 -0400

    web server assignment

---
+++ first_server.rb
@@ -0,0 +1,17 @@
+require 'socket'
+# IP address is 0.0.0.0 and it's on port 8080:
+server = TCPServer.new("0.0.0.0", 8080)
+loop do
+    connection = server.accept   # Open connection
+    inputline = connection.gets  # Read from connection
+    until connection.gets == nil
+      connection.puts connection.gets
+    end
+    connection.print "HTTP/1.1 200 OK\r\n" +
+      "Content-Type: text/plain\r\n" +
+      "Connection: close\r\n\r\n"
+
+    connection.puts File.open("page.html", "r").readlines
+    connection.puts "We received a request that looked like #{inputline} with data"
+    connection.close             # Close connection
+end
