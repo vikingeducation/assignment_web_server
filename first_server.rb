b5546d5afb1555acda1da398066733306bd5848c
Alex Lach <alexglach@gmail.com>
2016-07-19 12:43:01 -0400

    finished up

--- first_server.rb
+++ first_server.rb
@@ -5,13 +5,10 @@ server = TCPServer.new("0.0.0.0", 8080)
 loop do
     connection = server.accept   # Open connection
     inputline = connection.gets  # Read from connection
-
-connection.print "HTTP/1.1 200 OK\r\n" +
-           "Content-Type: text/plain\r\n" +
-           "Connection: close\r\n\r\n"
-
-
-
-   File.open('helloworld.html').each {|item| connection.puts item } 
+    
+    connection.print "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
+               "Connection: close\r\n\r\n"
+    File.open('helloworld.html').each {|item| connection.puts item } 
     connection.close             # Close connection
 end
