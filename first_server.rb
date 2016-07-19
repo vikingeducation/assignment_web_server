0a195769c592033a15e23116c84df96063b62c12
Leo Ahnn <leoahnn@gmail.com>
2016-07-19 12:12:10 -0400

    initialized server

---
+++ first_server.rb
@@ -0,0 +1,21 @@
+require 'socket'
+# IP address is 0.0.0.0 and it's on port 8080:
+server = TCPServer.new("0.0.0.0", 8080)
+loop do
+    connection = server.accept   # Open connection
+    inputline = connection.gets  # Read from connection
+
+
+    connection.print "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
+               "Connection: close\r\n\r\n"
+
+    File.readlines("index.html").each do |line|
+      connection.puts line
+    end
+
+    connection.puts "We received a request that looked like #{ inputline }"
+    connection.puts "Hello World"   # Write into connection
+    connection.puts "This is a test"
+    connection.close             # Close connection
+end
