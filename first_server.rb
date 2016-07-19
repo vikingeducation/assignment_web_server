f90b3e4f207df0c59703d317bab986d99326e932
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-19 09:37:26 -0700

    updated with httyparty requests

--- first_server.rb
+++ first_server.rb
@@ -1,22 +1,24 @@
+
 require 'socket'
 # IP address is 0.0.0.0 and it's on port 8080:
 server = TCPServer.new("0.0.0.0", 8080)
 loop do
     connection = server.accept   # Open connection
-    inputline = connection.gets  # Read from connection
+    inputline = connection.gets #|| connection.post  # Read from connection
+    # postline = connection.post
     connection.print "HTTP/1.1 200 OK\r\n" +
             "Content-Type: text/plain\r\n" +
             "Connection: close\r\n\r\n"
-    connection.print "We received a request that looked like #{ inputline } and contained the following data: generic parameter"
+    # connection.print "We received a request that looked like #{ inputline } and contained the following data: #{inputline.body}"
 
     file = File.readlines("hello.html")
-    
+
      file.each do |line|
        connection.puts line
      end
 
     connection.puts "Hello World"   # Write into connection
-    connection.puts inputline
+    # connection.puts inputline
     connection.close             # Close connection
 end
 
