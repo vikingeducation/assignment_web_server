7af996c1465b457c54c00e950a6e59c1370af87c
David Watts <davidjeremywatts@gmail.com>
2016-07-19 16:38:25 -0700

    add first web server

---
+++ first_server.rb
@@ -0,0 +1,16 @@
+require 'socket'
+# IP address is 0.0.0.0 and it's on port 8080:
+server = TCPServer.new("0.0.0.0", 8080)
+loop do
+    connection = server.accept   # Open connection
+    inputline = connection.gets  # Read from connection
+    connection.print "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Connection: close\r\n\r\n"
+    file = File.read("test.html").split("\n")
+    file.each do |line|
+      connection.puts line
+    end
+    connection.puts inputline
+
+
+    connection.close             # Close connection
+end
