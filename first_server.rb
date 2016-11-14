c3f3ad46e103f1440a82edb5f2456b3be9df6c95
Sampson Crowley <sampsonsprojects@gmail.com>
2016-11-14 11:20:42 -0700

    working post request

--- first_server.rb
+++ first_server.rb
@@ -1,18 +1,26 @@
 require 'socket'
-require 'httparty'
 
 server = TCPServer.new("0.0.0.0", 8080)
 loop do
-  connection = server.accept
-  inputline = connection.gets
+  client = server.accept       # Wait for a client to connect
+  method, path = client.gets.split                    # In this case, method = "POST" and path = "/"
+  headers = {}
+  while line = client.gets.split(' ', 2)              # Collect HTTP headers
+    break if line[0] == ""                            # Blank line means no more headers
+    headers[line[0].chop] = line[1].strip             # Hash headers by type
+  end
+  data = client.read(headers["Content-Length"].to_i)  # Read the POST data as specified in the header
 
+  puts data 
   # connection.puts "Hello World"
   # connection.print "HTTP/1.1 302 Found\r\n" +
   #          "Content-Type: text/plain\r\n" +
   #          "Connection: close\r\n\r\n"
   file = File.open("hello.html")
   file.each do |line|
-    connection.puts line
+    client.puts line
   end
-  connection.close
+  client.puts data
+  client.close
+
 end
