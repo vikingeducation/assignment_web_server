dadc967f84d685bba6e6bb7564255744963cc5dd
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-14 11:42:01 -0700

    Yay POST requests

--- client.rb
+++ client.rb
@@ -1,4 +1,9 @@
-require 'open-uri'
-open('http://localhost:8080') do |f|
-    f.each_line{|line| puts line}
-  end
+require 'httparty'
+
+p HTTParty.post('http://0.0.0.0:8080', {
+  body: {text: 'test'}.to_json
+  })
+
+# open('http://localhost:8080', our_hash) do |f|
+#   f.each_line {|line| puts line}
+# end
--- first_server.rb
+++ first_server.rb
@@ -1,10 +1,15 @@
 require "socket"
 
 server = TCPServer.new("0.0.0.0", 8080)
-loop do 
+loop do
   connection = server.accept
   inputline = connection.gets
 
+  connection.print "HTTP/1.1 200 OK\r\n" +
+           "Content-Type: text/plain\r\n" +
+           "Connection: close\r\n\r\n"
+
+  connection.puts inputline
   connection.puts File.readlines("hello.html")
   connection.close
 end
