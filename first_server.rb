45f7934fda0fc9aac3c9a5631d697002e35771f4
Visiona <darbis@gmail.com>
2017-01-18 19:12:53 +0000

    Created web server script

--- first_server.rb
+++ first_server.rb
@@ -1,12 +1,27 @@
 require 'socket'
 
 server = TCPServer.new("0.0.0.0", 8080)
-loop do
-  connection = server.accept
-  inputline = connection.gets
+begin
 
+  loop do
+    connection = server.accept
+    inputline = connection.gets
+    read = File.read("test.html")
+    parameter = inputline.split
 
-  connection.puts "Hello World"
-  connection.puts File.read("test.html")
-  connection.close
+    connection.print "HTTP/1.1 200 OK\r\n" +
+               "Content-Type: text/plain\r\n" +
+               "Connection: close\r\n\r\n"
+
+    connection.print "We received a request that looked like #{ inputline } and contained the following data: #{ parameter[1]} \n"
+
+    connection.puts "Hello World"
+    connection.puts File.read("test.html")
+    connection.close
+
+  end
+
+rescue SystemExit, Interrupt
+  puts "\n"
+  puts "Goodbye!"
 end
