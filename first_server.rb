bb8abd47495926a55b9f31a3ac448642eafa4e11
Roy Chen <roychenlei@gmail.com>
2017-04-28 14:54:50 +0800

    modified server to output test file

--- first_server.rb
+++ first_server.rb
@@ -2,12 +2,18 @@ require 'socket'
 
 server = TCPServer.new('127.0.0.1', 8080)
 
+# read in test file
+filename = './helloer_worlder.html'
+lines = File.readlines(filename)
+
 loop do
   connection = server.accept  # open connection
   inputline = connection.gets # read from connection
 
   puts inputline  # see what client sent in the connection
 
-  connection.puts "Hello World!"  # write into connection
+  # return contents of our file
+  lines.each { |line| connection.puts line }
+
   connection.close  # close connection
 end
