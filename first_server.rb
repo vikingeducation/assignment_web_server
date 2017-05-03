c7941f5e60748ad2e71993a258b824e7696f619c
Ann Allan <ann.cristina.allan@gmail.com>
2017-05-03 19:47:29 -0400

    Refactor server

--- first_server.rb
+++ first_server.rb
@@ -1,16 +1,36 @@
 require 'socket'
 # IP address is 0.0.0.0 and it's on port 8080
-server = TCPServer.new("0.0.0.0", 8080)
-loop do
-  connection = server.accept #open connection
-  inputline = connection.gets #read from connection
-  puts inputline
-  arr = "We received a request that looked like #{inputline} and contained the following data: #{} "
+server = TCPServer.new("localhost", 8080)
+ABSPATH = File.expand_path(File.dirname(__FILE__))
 
-  connection.print "HTTP/1.1 200 OK\r\n" +
-                              "Content-Type: text/plain\r\n" +
-                              "Connection: close\r\n\r\n"
-  connection.puts arr # write into connection
-  connection.close #close connection
+file = File.read("#{ABSPATH}/index.html")
+begin
+  loop do
+    connection = server.accept #open connection
+    inputline = connection.gets #read from connection
+    puts inputline
+    params = {}
+    query = inputline.split('?')[1]
+    if query
+      query = query.split(' ')[0]
+      query.split('&').each do |param|
+        param = param.split('=')
+        key = param[0]
+        value = param[1]
+        params[key] = value
+      end
+    end
 
+    response = file
+    response_headers = ["HTTP/1.1 200 OK\r\n", "Content-Type: text/plain\r\n", "Connection :close\r\n\r\n"].join
+    arr = "We received a request that looked like #{inputline} and contained the following data: #{} "
+
+    connection.print(response_headers)
+    connection.puts(response)
+    connection.puts arr # write into connection
+    connection.close #close connection
+  end
+rescue SystemExit, Interrupt
+  puts "\n"
+  puts "Ending server connection"
 end
