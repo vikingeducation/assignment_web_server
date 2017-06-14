5b36c3e469291f71e7e59aae48d65f68b03a1871
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-06-14 11:51:09 -0700

    finished first_server

--- first_server.rb
+++ first_server.rb
@@ -3,8 +3,17 @@ require 'socket'
 server = TCPServer.new("0.0.0.0", 8080)
 loop do
   connection = server.accept
-  inputline = connection.gets
+  request_info = ""
+  while inputline = connection.gets
+    request_info += inputline
+    break if inputline == "\r\n"
+  end
+
+  connection.print "HTTP/1.1 200 OK\r\n" +
+  "Content-Type: text/html\r\n" +
+  "Connection: close\r\n\r\n"
 
   connection.puts File.readlines('html_page.html')
+  connection.puts "We received request #{request_info}"
   connection.close
 end
