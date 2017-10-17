2e8b3ea628918cdb077c47d379caf89e87a5e169
anne <richardson.ae@gmail.com>
2017-10-17 15:01:36 -0500

    Adjust indenting for clarity

--- first_server.rb
+++ first_server.rb
@@ -6,17 +6,19 @@ port = 8080
 server = TCPServer.new(host, port)
 puts "Listening at http://#{host}:#{port}","Ctrl+C to quit"
 
+
 loop do
     # Open connection
     connection = server.accept
 
-    # Read from connection
+    # Read from connection & output to console
     inputline = connection.gets
+    puts inputline
 
     # Response header
-    connection.print "HTTP/1.1 200 OK\r\n" +
-           "Content-Type: text/plain\r\n" +
-           "Connection: close\r\n\r\n"
+    connection.print("HTTP/1.1 200 OK\r\n" +
+                     "Content-Type: text/plain\r\n" +
+                     "Connection: close\r\n\r\n")
 
     # Open the file that will be rendered
     file = File.readlines("index.html")
