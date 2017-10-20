64f0c7da72c04b9065f3ab88f731babdbed7bc65
Visiona <darbis@gmail.com>
2017-10-20 21:21:13 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-Dariusz Biskupski
+#  A very simple Web Sever in Ruby
 
-# assignment_web_server
-Ready to serve, milord!
 
-[A Ruby web server project from the Viking Code School](http://www.vikingcodeschool.com)
+This is a quick project to practice how the Web works. I have built here mini server in Ruby and also a mini CLI-based client which will send server requests in order to use the client to talk to your server.
+
+
+## Getting Started
+
+If you want to quick run some the examples to see the code in action, and you have installed Ruby and Rails, run
+```
+$ ruby first_server.rb
+```
+
+This will run a loop/server in CLI and at the following address in the browser at http://0.0.0.0:8080/
+
+## Authors
+
+* **Dariusz Biskupski** - *Initial work* - https://dariuszbiskupski.com
+
+
+## Acknowledgments
+
+It is the assignment created for [Viking Code School](https://www.vikingcodeschool.com/)
--- first_server.rb
+++ first_server.rb
@@ -6,14 +6,26 @@ begin
   loop do
     connection = server.accept
     inputline = connection.gets
-    read = File.read("test.html")
-    parameter = inputline.split
+    parameter = inputline.split("?")[1]
+
+    params = {}
+
+    if parameter
+      parameter = query.spli(" ")[0]
+      parameter.split("&").each do |param|
+        param = param.split("=")
+        key = param[0]
+        value = param[1]
+        params[key] = value
+      end
+    end
+
 
     connection.print "HTTP/1.1 200 OK\r\n" +
                "Content-Type: text/plain\r\n" +
                "Connection: close\r\n\r\n"
 
-    connection.print "We received a request that looked like #{ inputline } and contained the following data: #{ parameter[1]} \n"
+    connection.print "We received a request that looked like #{ inputline } and contained the following data: query: #{parameter} and params: #{ params} \n"
 
     connection.puts "Hello World"
     connection.puts File.read("test.html")
